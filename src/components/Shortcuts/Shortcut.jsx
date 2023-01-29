60f32cb214ed250972137264364ef3c67f24a95d
Puskass <cosovictarik711@gmail.com>
2023-01-29 20:39:49 +0100

    update

--- src/components/Shortcuts/Shortcut.jsx
+++ src/components/Shortcuts/Shortcut.jsx
@@ -9,10 +9,30 @@ const Shortcut = () => {
       <div className="row-1">
         
         <div className="icon icon-1">
-        <YouTubeIcon />
+        <YouTubeIcon className='center'/>
         <p className="icon-name">YouTube</p>
         </div>
        
+        <div className="icon icon-2">
+        <YouTubeIcon />
+        <p className="icon-name">Pirates bay</p>
+        </div>
+
+        <div className="icon icon-3">
+        <YouTubeIcon />
+        <p className="icon-name">Mp3 Converter</p>
+        </div>
+      
+        <div className="icon icon-4">
+        <YouTubeIcon />
+        <p className="icon-name">academind</p>
+        </div>
+
+        <div className="icon icon-5">
+        <YouTubeIcon />
+        <p className="icon-name">hackthebox</p>
+        </div>
+
       </div>
     </div>
   
