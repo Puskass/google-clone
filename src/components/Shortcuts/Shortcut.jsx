709c592298c04d93438d0d8f512db44d216e80ac
Puskass <cosovictarik711@gmail.com>
2023-01-30 18:37:16 +0100

    Googlet Home Page

--- src/components/Shortcuts/Shortcut.jsx
+++ src/components/Shortcuts/Shortcut.jsx
@@ -1,36 +1,66 @@
 import React from 'react';
 import './Shortcut.css';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import FindReplaceIcon from '@mui/icons-material/FindReplace';
 
 const Shortcut = () => {
   return (
     
     <div className='shortcut'>
-      <div className="row-1">
+      <div className="row row-1">
         
-        <div className="icon icon-1">
-        <YouTubeIcon className='center'/>
+        <div className="icon">
+        <YouTubeIcon />
         <p className="icon-name">YouTube</p>
         </div>
        
-        <div className="icon icon-2">
-        <YouTubeIcon />
+        <div className="icon">
+        <FindReplaceIcon />
         <p className="icon-name">Pirates bay</p>
         </div>
 
-        <div className="icon icon-3">
-        <YouTubeIcon />
-        <p className="icon-name">Mp3 Converter</p>
+        <div className="icon">
+        <FindReplaceIcon />
+        <p className="icon-name icon-3">Mp3 Conv.</p>
         </div>
       
-        <div className="icon icon-4">
-        <YouTubeIcon />
+        <div className="icon">
+        <FindReplaceIcon />
         <p className="icon-name">academind</p>
         </div>
 
-        <div className="icon icon-5">
-        <YouTubeIcon />
+        <div className="icon">
+        <FindReplaceIcon />
         <p className="icon-name">hackthebox</p>
+        </div>
+
+      </div>
+
+      <div className="row row-2">
+        
+        <div className="icon">
+        <FindReplaceIcon />
+        <p className="icon-name">GitHub</p>
+        </div>
+       
+        <div className="icon">
+        <FindReplaceIcon />
+        <p className="icon-name">Spotify</p>
+        </div>
+
+        <div className="icon">
+        <FindReplaceIcon />
+        <p className="icon-name">Google</p>
+        </div>
+      
+        <div className="icon">
+        <FindReplaceIcon />
+        <p className="icon-name">npm</p>
+        </div>
+
+        <div className="icon">
+        <FindReplaceIcon />
+        <p className="icon-name">netflix.com</p>
         </div>
 
       </div>
